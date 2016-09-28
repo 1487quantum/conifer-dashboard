ed861fafb4bde11f64b36a260ecee785c0725cae
Photon <1487quantum@users.noreply.github.com>
2016-09-28 03:24:00 -0400

    Added link to cmd directory

--- web_server.js
+++ web_server.js
@@ -1,6 +1,6 @@
 var fs = require("fs");
-var host = "127.0.0.1";
-var port = 1337;
+var host = "0.0.0.0";
+var port = 8080;
 var express = require("express");
 console.log("Starting Web Server...");
 var app = express();
@@ -8,6 +8,11 @@ app.use(express.static(__dirname + "/public")); //use static files in ROOT/publi
 
 app.get("/", function(request, response){ //root dir
     response.send("Loading page...");
+});
+
+app.get("/cmd", function(request, response){ //control dir
+    response.send("Loading command page...");
+	app.use(express.static(__dirname + "/public/cmd"));
 });
 
 app.listen(port, host);
